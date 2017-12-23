bcc1c26755ba1b5478aa7d85d026b4376739978f
unknown <danieljosephrodrigues@gmail.com>
2017-12-23 10:52:37 -0800

    adding connection string

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True #displays runtime errors in the browser
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://golfapp:golfapp@localhost:8889/golfapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://cqx01uvb2wce9v5y:kus9g2j2437c835u@tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ypgr5fx3g3icm84n'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
